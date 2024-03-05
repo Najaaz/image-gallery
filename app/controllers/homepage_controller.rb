d5a349c61408120b9fd2f2fef44cb0330994aa88
Najaaz <najaaznabhan@gmail.com>
2024-03-05 20:16:56 +0000

    [refactor]: Cards were made more modular and dynamic

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,10 @@
 class HomepageController < ApplicationController
     def index
         @greeting = "Welcome to my custom page!"
+        @cards = [
+            { title: "Gallery 101", description: "This is the first gallery." },
+            { title: "Gallery 201", description: "This is the second gallery." },
+            { title: "Gallery 301", description: "This is the third gallery." }
+        ]
     end
 end
