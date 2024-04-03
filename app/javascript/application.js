ac1046d692857f309683b2371cf40375a8952e20
Najaaz <najaaznabhan@gmail.com>
2024-04-03 17:14:09 +0530

    [feat]: add the functionality for a carousel

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+document.addEventListener('show.bs.modal', function (event) {
+    var button = event.relatedTarget; // Button that triggered the modal
+    var buttonId = button.getAttribute('data-button-id'); // Extract info from data-* attributes
+
+    // Update the modal's content with the button ID.
+    var modal = event.target;
+    // Remove the class active from all the children
+    Array.from(modal.querySelector('.carousel-inner').children).forEach((child) => {
+        child.classList.remove('active');
+    });
+    
+    modal.querySelector('.carousel-inner').children[buttonId].classList.add('active')
+}, false);
+
